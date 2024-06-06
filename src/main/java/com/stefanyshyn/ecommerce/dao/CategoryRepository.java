6802756ffd6a3a6c2b20cc676c5077cee56c53d3
roman <romanpz051@gmail.com>
2024-06-06 19:49:40 +0200

    0.0.1 init-back

---
+++ src/main/java/com/stefanyshyn/ecommerce/dao/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.stefanyshyn.ecommerce.dao;
+
+import com.stefanyshyn.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path ="product-category" )
+public interface CategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
