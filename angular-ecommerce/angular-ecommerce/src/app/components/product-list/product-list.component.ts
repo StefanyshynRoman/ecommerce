f9fcd38f2ef6f34a7229cf92f9997b811c277f1d
roman <romanpz051@gmail.com>
2024-06-10 12:25:59 +0200

    0.0.5 eslint+services-front

--- angular-ecommerce/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
-
-}
+export class ProductListComponent {}
