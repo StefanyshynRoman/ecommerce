06e5835b027b01a33df9eeda11e9ce9c15059bb1
roman <romanpz051@gmail.com>
2024-06-11 09:38:51 +0200

    0.0.6 table-front

--- angular-ecommerce/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
+  //templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {}
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.listProducts();
+  }
+
+  private listProducts() {
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data;
+    });
+  }
+}
