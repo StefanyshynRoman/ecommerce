30aa5dc6eb65a2bc9afcbfc33699cbf1ac503994
roman <romanpz051@gmail.com>
2024-06-20 19:51:14 +0200

    0.0.15 cart-details

--- angular-ecommerce/angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/angular-ecommerce/src/app/app.module.ts
@@ -14,12 +14,12 @@ import { CartStatusComponent } from './components/cart-status/cart-status.compon
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 const routes: Routes = [
+  { path: 'cart-details', component: CartDetailsComponent },
   { path: 'products/:id', component: ProductDetailsComponent },
   { path: 'search/:keyword', component: ProductListComponent },
   { path: 'category/:id/:name', component: ProductListComponent },
   { path: 'category', component: ProductListComponent },
   { path: 'products', component: ProductListComponent },
-  { path: 'cart-details', component: CartDetailsComponent },
   { path: '', redirectTo: '/products', pathMatch: 'full' },
   { path: '**', redirectTo: '/products', pathMatch: 'full' },
 ];
