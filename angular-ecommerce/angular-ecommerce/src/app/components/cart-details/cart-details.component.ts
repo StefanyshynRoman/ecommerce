b59bbc894b3e2bec69205451867402401705131e
roman <romanpz051@gmail.com>
2024-06-21 19:13:38 +0200

    0.0.15 cart-decrem&increm

--- angular-ecommerce/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ angular-ecommerce/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -8,7 +8,7 @@ import { CartService } from '../../services/cart.service';
   styleUrls: ['./cart-details.component.css'],
 })
 export class CartDetailsComponent implements OnInit {
-  carItems: CartItem[] = [];
+  cartItems: CartItem[] = [];
   totalPrice = 0;
   totalQuantity = 0;
   constructor(private cartService: CartService) {}
@@ -17,11 +17,23 @@ export class CartDetailsComponent implements OnInit {
   }
 
   private listCartDetails() {
-    this.carItems = this.cartService.cartItems;
+    this.cartItems = this.cartService.cartItems;
     this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
     this.cartService.totalQuantity.subscribe(
       (data) => (this.totalQuantity = data),
     );
     this.cartService.computeCartTotals();
+  }
+
+  incrementQuantity(theCartItem: CartItem) {
+    this.cartService.addToCart(theCartItem);
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    this.cartService.decrementQuantity(theCartItem);
+  }
+
+  remove(thrCartItem: CartItem) {
+    this.cartService.remove(thrCartItem);
   }
 }
