4a91fd00a207e055583520ef939f13543fa0b9e6
roman <romanpz051@gmail.com>
2024-07-01 17:12:02 +0200

    0.1.01 OKTA-login

---
+++ angular-ecommerce/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oai1o7vkyGu9wVgf5d7',
+    issuer: 'https://dev-57243594.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
